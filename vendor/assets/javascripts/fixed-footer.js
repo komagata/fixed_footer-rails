d4f1283f39c944a847c8c2038f724e97a26ab5d2
Masaki Komagata <komagata@gmail.com>
2017-02-21 19:10:43 +0900

    Refactor

--- vendor/assets/javascripts/fixed-footer.js
+++ vendor/assets/javascripts/fixed-footer.js
@@ -1,15 +1,14 @@
 window.FixedFooter = function(id) {
-  var addEvent, checkFontSize, footer, footerFixed;
-  footer = document.getElementById(id);
-  if (!footer) {
-    return null;
-  }
-  footerFixed = function() {
-    var dh, fh, ft, wh;
-    dh = document.getElementsByTagName("body")[0].clientHeight;
+  var footer = document.getElementById(id);
+  if (!footer) { return null; }
+
+  var footerFixed = function() {
+    var footer = document.getElementById(id);
+    var dh = document.getElementsByTagName("body")[0].clientHeight;
     footer.style.top = "0px";
-    ft = footer.offsetTop;
-    fh = footer.offsetHeight;
+    var ft = footer.offsetTop;
+    var fh = footer.offsetHeight;
+    var wh;
     if (window.innerHeight) {
       wh = window.innerHeight;
     } else if (document.documentElement && document.documentElement.clientHeight !== 0) {
@@ -17,29 +16,11 @@ window.FixedFooter = function(id) {
     }
     if (ft + fh < wh) {
       footer.style.position = "relative";
-      return footer.style.top = wh - fh - ft - 1 + "px";
+      footer.style.top = wh - fh - ft - 1 + "px";
     }
   };
-  checkFontSize = function(callback) {
-    var checkBoxSize, defHeight, e, s;
-    e = document.createElement("div");
-    s = document.createTextNode("S");
-    checkBoxSize = function() {
-      var defHeight;
-      if (defHeight !== e.offsetHeight) {
-        callback();
-        return defHeight = e.offsetHeight;
-      }
-    };
-    e.appendChild(s);
-    e.style.visibility = "hidden";
-    e.style.position = "absolute";
-    e.style.top = "0";
-    document.body.appendChild(e);
-    defHeight = e.offsetHeight;
-    return setInterval(checkBoxSize, 1000);
-  };
-  addEvent = function(elm, listener, fn) {
+
+  var addEvent = function(elm, listener, fn) {
     var e;
     try {
       elm.addEventListener(listener, fn, false);
@@ -48,6 +29,7 @@ window.FixedFooter = function(id) {
       elm.attachEvent("on" + listener, fn);
     }
   };
-  checkFontSize(footerFixed);
-  return addEvent(window, "resize", footerFixed);
+
+  footerFixed();
+  addEvent(window, "resize", footerFixed);
 };
